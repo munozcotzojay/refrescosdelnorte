b3679bf1a0d3f91384939802537e7dfa25785634
munozcotzojay <91991291+munozcotzojay@users.noreply.github.com>
2021-10-05 15:51:00 -0600

    Add files via upload

---
+++ acceso.js
@@ -0,0 +1,10 @@
+function go(){
+if (document.formingreso.usuario.value=='lisy' && document.formingreso.contraseña.value=='examen');  
+    {
+    document.formingreso.submit();
+    }
+else
+    {
+    alert("Ingrese su usuario y contraseña correctamente.");
+    }
+}
